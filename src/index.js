9f3458fff85a4655407d6535ca9ccf3ae3bf1df0
Aigerim <52836936+kydyrbekova@users.noreply.github.com>
2021-02-03 11:48:57 +0600

    Update index.js
    Add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArray = str.split('');
+  var stack  = [];
+  var countBrackets = 0;
+  while(countBrackets<str.length){
+	stack.push(strArray[countBrackets]);
+	for(var i=0; i<bracketsConfig.length; i++){
+		if(stack[stack.length-2]== bracketsConfig[i][0] && stack[stack.length-1] == bracketsConfig[i][1]){ stack.pop(); stack.pop();}	
+	}	
+	countBrackets++;
+  }
+  return stack.length? false:true;
 }
+
